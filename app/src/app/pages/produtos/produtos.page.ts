a0592090cf42add90713059df090cff03405bb69
Leonardo Sena <leeosena21@gmail.com>
2020-05-13 09:42:17 -0300

    Carregando imagem dos produtos

--- app/src/app/pages/produtos/produtos.page.ts
+++ app/src/app/pages/produtos/produtos.page.ts
@@ -3,6 +3,8 @@ import { ProdutoDTO } from 'src/app/models/produto.dto';
 import { NavController, NavParams } from '@ionic/angular';
 import { ProdutoService } from 'src/app/services/domain/produto.service';
 import { ActivatedRoute } from '@angular/router';
+import { OutgoingMessage } from 'http';
+import { API_CONFIG } from 'src/app/config/api.config';
 
 @Component({
   selector: 'app-produtos',
@@ -30,11 +32,23 @@ export class ProdutosPage implements OnInit {
     this.produtoService.findByCategoria(categoria_id)
       .subscribe(response => {
         this.items = response['content'];
+        this.loadImageUrls();
       },
       error => {});
     }
 
   back(){
     this.navCtrl.back();
+  }
+
+  loadImageUrls(){
+    for(var i=0; i<this.items.length; i++){
+      let item = this.items[i];
+      this.produtoService.getSmallImageFromBucket(item.id)
+      .subscribe(response => {
+        item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
+      },
+      error =>{});
+    }
   }
 }
